968d42bf4149d5298ee9eddac769f5f14f805348
Sannebeast <jimk0001@hz.nl>
2025-06-08 11:50:21 +0200

    installed laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,39 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('home');
+    }
+    return redirect()->route('login');
+});
 
-/*
- * The home routes
- */
-Route::get('/', [TaskController::class, 'index'])->name('home');
-
-/*
- * The resource routes
- */
-Route::resource('tasks', TaskController::class)
-    ->except(['index']); // removed this route because this is moved to the home route
-Route::post('tasks/{task}/complete', [TaskController::class, 'complete'])
-    ->name('tasks.complete');
-Route::resource('posts', PostController::class);
-Route::resource('projects', ProjectController::class);
-
-/*
- * Route that shows the about page. This handler just returns the about view.
- */
 Route::view('/about', 'about')->name('about');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [TaskController::class, 'index'])->name('home');
+
+    Route::resource('tasks', TaskController::class)->except(['index']);
+    Route::post('tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
+    Route::resource('posts', PostController::class);
+    Route::resource('projects', ProjectController::class);
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+});
+
+require __DIR__.'/auth.php';
